68dfb96748ddd4511e223ad908d4d085c6287ce6
albuquerquealdry <albuquerquealdry@gmail.com>
2022-05-07 03:01:40 -0300

    tes

--- teste.py
+++ teste.py
@@ -1,7 +1,9 @@
 import os
 
+typeservice= ""
 env = "JOB_NAME"
 
 
 os.system("git clone https://github.com/albuquerquealdry/{}.git".format(env))
-os.system("cd {} && ls -lh >> README.md ".format(env))
+os.system(""" cd {} && ls -lh >> README.md && git add . && git commit -m"teste" && git push """.format(env))
+
